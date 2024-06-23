02f247a1c6d841c7d6a072a3f0e04622a9f6373e
Anna Ha <its.haanna@gmail.com>
2024-06-23 13:10:24 +0900

    Update: audio handling, mouse gesture movement

--- lib/tiny_violin.dart
+++ lib/tiny_violin.dart
@@ -12,13 +12,34 @@ class TinyViolin extends StatefulWidget {
 
 class _TinyViolinState extends State<TinyViolin> {
   Offset pointer = Offset.zero;
+  bool isPlayerOn = false;
   final player = AudioPlayer(playerId: 'tinyviolin');
   Future<void> playViolin() async {
     await player.play(AssetSource('audio/sad-violin.mp3'));
+    setState(() {
+      isPlayerOn = true;
+    });
   }
 
-  Future<void> stopViolin() async {
+  void stopViolin() {
     player.stop();
+    setState(() {
+      isPlayerOn = false;
+    });
+  }
+
+  Future<void> handleViolin() async {
+    if (isPlayerOn) {
+      stopViolin();
+    } else {
+      await playViolin();
+    }
+  }
+
+  void _updatePointerPosition(PointerEvent event) {
+    setState(() {
+      pointer = event.position;
+    });
   }
 
   @override
@@ -28,47 +49,47 @@ class _TinyViolinState extends State<TinyViolin> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: MouseRegion(
-        cursor: SystemMouseCursors.none,
-        onHover: (eve) {
-          setState(() {
-            pointer = eve.position;
-          });
-        },
-        child: Stack(
-          children: [
-            GestureDetector(
-              onTap: () {
-                playViolin();
-              },
-              onSecondaryTap: () {
-                stopViolin();
-              },
-              child: const Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Image(
-                        width: 500,
-                        image: AssetImage('assets/violin.png'),
+      body: Listener(
+        onPointerMove: _updatePointerPosition,
+        child: MouseRegion(
+          cursor: SystemMouseCursors.none,
+          onHover: (eve) {
+            setState(() {
+              pointer = eve.position;
+            });
+          },
+          child: Stack(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  handleViolin();
+                },
+                child: const Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Image(
+                          width: 500,
+                          image: AssetImage('assets/violin.png'),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-            AnimatedPositioned(
-              duration: const Duration(milliseconds: 100),
-              left: pointer.dx - 100,
-              top: pointer.dy - 100,
-              child: const Image(
-                width: 250,
-                image: AssetImage('assets/violin-bow.png'),
+              AnimatedPositioned(
+                duration: const Duration(milliseconds: 100),
+                left: pointer.dx - 100,
+                top: pointer.dy - 100,
+                child: const Image(
+                  width: 250,
+                  image: AssetImage('assets/violin-bow.png'),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
